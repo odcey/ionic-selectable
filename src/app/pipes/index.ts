dc286f5c1e8a8eceff5a7d820544ac9a374b0af7
Sébastien Ceylan <ceylanseb@gmail.com>
2020-04-21 18:25:23 +0200

    [Dist] trying to build project with fix

--- src/app/pipes/index.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { WikiUrlPipe } from './wiki-url.pipe';
-
-@NgModule({
-  declarations: [WikiUrlPipe],
-  imports: [],
-  exports: [WikiUrlPipe]
-})
-export class PipesModule { }
--- src/app/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './country.interface';
-export * from './country.type';
-export * from './location.interface';
-export * from './location.type';
-export * from './port.interface';
-export * from './port.type';
